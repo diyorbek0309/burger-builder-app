4d43c60ba7822b2b3d040cbbb97cfa9beb1a530d
diyorbek0309 <diyorbekolimov2000@gmail.com>
2021-04-18 15:23:26 +0500

    finished basic version

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,13 @@
-import React from 'react';
+import React from "react";
 
-import Aux from '../../hoc/Aux';
-import classes from './Layout.css';
+import Aux from "../../../hoc/Auxilliary/Auxillary";
+import classes from "./Layout.css";
 
-const layout = ( props ) => (
-    <Aux>
-        <div>Toolbar, SideDrawer, Backdrop</div>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
+const layout = (props) => (
+  <Aux>
+    <div>Toolbar, SideDrawer, Backdrop</div>
+    <main className={classes.Content}>{props.children}</main>
+  </Aux>
 );
 
 export default layout;
